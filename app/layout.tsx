bf00f16cb50f99afcbb450bb7e0f471cdc2fd3fc
OscarJGarcia <oscar.garciag@ServiciosNutresa.onmicrosoft.com>
2024-08-12 16:54:38 -0500

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { Metadata } from "next";
+import "./globals.css";
+import Provider from "./Provider";
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metada: Metadata = {
+  title: "LiveDocs",
+  description: "Your go-to collaborate editor",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { colorPrimary: "#3371FF", fontSize: "16px" },
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          <Provider>{children}</Provider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
